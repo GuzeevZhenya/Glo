1bc69c1645b7904133cd7b44f14e4624616b3612
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-08 14:04:33 +0300

    done

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-let arr = ['123','435','213','5423','6523','234','43'];
-
-for (let i = 0; i < arr.length; i++){
-	if (arr[i][0] === '2' || arr[i][0] === '4') {
-		console.log(arr[i]);
-	}
+function getRandomNumber(number){
+	return Math.floor(Math.random()*100);
 }
 
-const dividers = el => {
-	let arr = [];
-	if (el !== 1) {
-			arr.push(1);
-			for (let j = 2; j * j <= el; j++) {
-					if (el % j === 0) {
-							arr.push(j);
-					}
-			}
-	}
-	arr.push(el);
-	return arr;
-}; 
 
-for (let i = 1; i <= 100; i++) {
-	const n = dividers(i);
-	if (n.length <= 2) {
-			console.log(`${i}: Простое число. Делители этого числа: ${n.join(', ')}`);
+function start (number){
+	let num = number;
+	function game(){
+		const userNumber = +prompt('Введите число от 1 до 100');
+		if(userNumber === null){
+			alert('До свидания')
+			return
+		}
+		if(userNumber ===num){
+			alert('Вы угадали число')
+		}else if(userNumber > num){
+			alert('Ваше число больше загаданного')
+			game();
+		}else if(userNumber < num){
+			alert('Ваше число меньше загаданного')
+			game();
+		}
+		else	{
+				alert('Введите число');
+				game();
+		}
 	}
+	game();
 }
 
-for (let i = 1; i <= 100; i++) {
-	console.log(`${i}: Делители этого числа: ${dividers(i).join(', ')}`);
-}
 
-console.log(arr.map(item => item).filter(item => item[0] == '2' || item[0] == '4'));
+start( getRandomNumber(100));
