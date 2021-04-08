ac04d66efc7910b319e18bd87a0a64bff3a7f91a
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-08 15:18:43 +0300

    fix

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ function getRandomNumber(number) {
 	return Math.floor(Math.random() * 100);
 }
 
+function isNum (number) {
+	return !isNaN(parseFloat(number)) && isFinite(number);
+};
+
 
 function start(number) {
 	let num = number;
 	let attempts = 3;
 
 	function game() {
+		
 		attempts--;
-
 		if (attempts < 0) {
 			if (confirm('Игра оконченна,повторить?')) {
 				start();
@@ -25,18 +29,26 @@ function start(number) {
 				return
 			}
 
-			if (userNumber === num) {
-				alert('Вы угадали число')
-			} else if (userNumber > num) {
-				alert(`Ваше число больше загаданного, ${attempts}`)
-				game();
-			} else if (userNumber < num) {
-				alert(`Ваше число меньше загаданного, ${attempts}`)
-				game();
-			} else {
-				alert('До свидания')
-				return
+			if (isNum(userNumber)) {
+			
+				if (userNumber === num) {
+					alert('Вы угадали число')
+				} else if (userNumber > num) {
+					alert(`Ваше число больше загаданного, ${attempts}`)
+					game();
+				} else if (userNumber < num) {
+					alert(`Ваше число меньше загаданного, ${attempts}`)
+					game();
+				} else {
+					alert('До свидания')
+					return
+				}
 			}
+			else {
+				alert('Введите число');
+				game();
+			}
+			
 		}
 	}
 	game();
