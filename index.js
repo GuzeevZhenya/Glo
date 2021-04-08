fcadd231242937a1f3a7c0b870aa307320b85fbc
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-08 14:45:40 +0300

    done

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-function getRandomNumber(number){
-	return Math.floor(Math.random()*100);
+function getRandomNumber(number) {
+	return Math.floor(Math.random() * 100);
 }
 
 
-function start (number){
+function start(number) {
 	let num = number;
-	function game(){
-		const userNumber = +prompt('Введите число от 1 до 100');
-		if(userNumber === null){
+
+	function game() {
+
+		const userNumber = +prompt('Введите число от 1 до 100 осталось');
+		console.log(userNumber);
+		if (userNumber === num) {
+			alert('Вы угадали число')
+		} else if (userNumber > num) {
+			alert('Ваше число больше загаданного')
+			game();
+		} else if (userNumber < num) {
+			alert('Ваше число меньше загаданного')
+			game();
+		} else {
 			alert('До свидания')
 			return
 		}
-		if(userNumber ===num){
-			alert('Вы угадали число')
-		}else if(userNumber > num){
-			alert('Ваше число больше загаданного')
-			game();
-		}else if(userNumber < num){
-			alert('Ваше число меньше загаданного')
-			game();
-		}
-		else	{
-				alert('Введите число');
-				game();
-		}
+		
 	}
-	game();
+ game();
 }
 
 
-start( getRandomNumber(100));
+start(getRandomNumber(100));
