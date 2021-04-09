e9578b05e3b7c3307ee942f684b726b1bcba3304
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-09 13:49:02 +0300

    done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let expenses = [];
 // на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth
 const getExpensesMonth = () => {
 	let sum = 0;
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 2; i++) {
 			expenses[i] = prompt('Введите обязательную статью расходов?');
 			sum += (() => {
 					let n = 0;
