9183ab91ba4ae26ebfe7631447a2671e5c36823f
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-09 12:43:17 +0300

    done

--- index.js
+++ index.js
@@ -1,59 +1,24 @@
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-};
+let month = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 
-function isNum (number) {
-	return !isNaN(parseFloat(number)) && isFinite(number);
-};
+let list = document.querySelector('.array-list');
 
 
-function start(number) {
-	let num = number;
-	let attempts = 10;
+let data = new Date();
+let weekDay = data.getDay();
 
-	function game() {
-	attempts--;
-		if (attempts < 0) {
-			if (confirm('Игра оконченна,повторить?')) {
-				start(getRandomNumber(100));
-			} else {
-				alert('До свидания');
-				return
-			}
-		} else {
-			const userNumber = prompt('Введите число от 1 до 100 осталось');
 
-			if (userNumber === null) {
-				alert('До свидания');
-				return
-			}
 
-			if (isNum(userNumber)) {
-				if (userNumber === num) {
-				} else if (userNumber > num) {
-					alert(`Ваше число больше загаданного, ${attempts}`)
-					game();
-				} else if (userNumber < num) {
-					alert(`Ваше число меньше загаданного, ${attempts}`)
-					game();
-				} else {
-					if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-						start();
-				} else {
-						alert('До свидания');
-						return;
-				}
-				}
-			}
-			else {
-				alert('Введите число');
-				game();
-			}
-			
-		}
+month.forEach((item, i) => {
+	let str = item;
+	if (i === weekDay-1) {
+		str =	`<b>${item}</b>`
+	} else {
+		str = `${item}`
 	}
-	game();
-}
+	if (i === 5 || i === 6) {
+		str = `<i>${item}</i>`
+	}
+	document.body.insertAdjacentHTML('beforeend', `<li>${str}</li>`)
+ })
 
 
-start(getRandomNumber(100));
