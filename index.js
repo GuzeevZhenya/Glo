4fee2a1c95c7d46e1b812aa24d111e0cad51388f
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-12 11:58:26 +0300

    done

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-// /*
-//     Задания по уроку №7
-// */
 
-// const isNumber = (n) => {
-// 	console.log('n: ', n);
-// 	console.log(parseFloat(n));
-// 	console.log(isFinite(n));
-// 	return !isNaN(parseFloat(n)) && isFinite(n);
-// };
-
-
-// let income = 'фриланс';
-// let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'интернет, такси, коммуналка');
-// let deposit = confirm('Есть ли у вас депозит в банке?');
-// let mission = 100000;
-// let period = 12;
-
-// // 1) Переписать функцию start циклом do while
-// do {
-// 	money = prompt('Ваш месячный доход?');
-// } while (!isNumber(money));
-
-// 
-// // 2) Добавить проверку что введённые данные являются числом, которые мы получаем 
-// // на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth
-
-
-// let expensesAmount = getExpensesMonth();
-
-
-
-// const accumulatedMonth = getAccumulatedMonth(money, expensesAmount);
-
-
-
-// const targetMonth = getTargetMonth(mission, accumulatedMonth);
-
-// const budgetDay = accumulatedMonth / 30;
-
-
-// console.log(addExpenses.toLocaleLowerCase().split(', '));
-// console.log('Обязательные расходы за месяц: ', expensesAmount);
-
-// console.log(`Цель будет достигнута за: ${targetMonth} месяцев`):
-// 	console.log(`Цель не будет достигнута`);
-
-// console.log('Бюджет на день: ', Math.floor(budgetDay));
-
-
-// console.log('getStatusIncome(): ', getStatusIncome(budgetDay));
-
-const isNumber = (n) => {
+let isNumber = function (n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
-};
+}
 
 function start() {
 	do {
@@ -79,7 +28,7 @@ let appData = {
 	moneyDeposit: 0,
 	addExpenses: [], // строка с перечислением дополнительных расходов
 	expenses: {}, //список обязательных статей расходов
-	deposit: false, // надичие депозита в банке
+	// deposit: false, // надичие депозита в банке
 	getExpensesMonth: function () {
 		appData.expensesMonth = 0;
 		for (let elem in appData.expenses) {
@@ -125,12 +74,12 @@ let appData = {
 		for (let i = 0; i < 2; i++) {
 			let str = '';
 			do {
-				str = prompt('Введите обязательную статью расходов?');
+				str = prompt('Введите обязательную статью расходов?','Налоги');
 			} while (!isString(str));
 			appData.expenses[str] = (() => {
 				let n = 0;
 				do {
-					n = prompt('Во сколько это обойдется?');
+					n = prompt('Во сколько это обойдется?',5000);
 				} while (!isNumber(n));
 				return +n;
 			})();
@@ -140,17 +89,17 @@ let appData = {
 	},
 	getInfoDeposit: () => {
 		if (appData.deposit) {
-			let n = 0;
-			do {
-				n = prompt('Какой годовой процент?', '10');
-			} while (!isNumber(n) && n > 0);
-			appData.precentDeposit = +n;
-			do {
-				appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
-			} while (!isNumber(n) && n > 0);
-			appData.moneyDeposit = +n;
+				let n = 0;
+				do {
+						n = prompt('Какой годовой процент?', '10');
+				} while (!isNumber(n) && n > 0);
+				appData.precentDeposit = +n;
+				do {
+						appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+				} while (!isNumber(n) && n > 0);
+				appData.moneyDeposit = +n;
 		}
-	},
+},
 	calcSavedMoney: function () {
 		return appData.budgetMonth * appData.period;
 	}
@@ -163,6 +112,8 @@ appData.getBudget();
 
 const targetMonth = appData.getTargetMonth();
 
+appData.getInfoDeposit();
+
 console.log('Расходы за месяц: ', appData.expensesMonth);
 console.log(targetMonth >= 0 ?
 	`Цель будет достигнута за: ${targetMonth} месяц(а/ев)` :
@@ -173,6 +124,7 @@ console.log('Наша программа включает в себя данны
 for (let elem in appData) {
 	console.log(elem, appData[elem]);
 }
-appData.getInfoDeposit();
+
 
 console.log('2) ' + appData.addExpenses.map((val, i) => val[0].toUpperCase() + val.slice(1)).join(', '));
+
