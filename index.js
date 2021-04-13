1890e0604b24dac5685eda6e879f394fcdaf0e09
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-13 14:15:47 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+let ulElement = document.querySelector('ul');
+let button = document.querySelector('button');
+let input = document.querySelector('input');
 
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	let inputValue = input.value;
+	let newElement = document.createElement('li');
+	newElement.innerHTML = inputValue;
+	ulElement.appendChild(newElement);
+	input.value = "";
+})
 
-let books = document.querySelectorAll('.book');
-let adv = document.querySelector('.adv');
-
-
-//Заменить картинку заднего фона на другую из папки image
-let bodyBackground = document.querySelector('body');
-bodyBackground.style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
-
-//справить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
-books[4].querySelector('a').textContent = 'Книга 3. this и Прототипы Объектов'
-
-//Удалить рекламу со страницы
-
-adv.remove();
