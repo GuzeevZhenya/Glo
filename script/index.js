cbff472e2a00b4909e52e29162c7a5a251220b6e
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-15 08:50:00 +0300

    done

--- script/index.js
+++ script/index.js
@@ -2,18 +2,21 @@ todoControl = document.querySelector('.todo-control')
 headerInput = document.querySelector('.header-input')
 todoList = document.querySelector('.todo-list');
 todoCompleted = document.querySelector('.todo-completed');
-todoContainer = document.querySelector('.todo-container')
-
 
 //Массив со значениями
-const todoData = [ 
+const todoData = [{
+		value: 'Сварить кофе',
+		completed: false,
+
+	},
+	{
+		value: 'Помыть посуду',
+		completed: true,
+	},
 ]
 
-
- 
 const render = function () {
-	
-
+ 
 
 	todoList.textContent = "";
 	todoCompleted.textContent = "";
@@ -25,69 +28,36 @@ const render = function () {
 		//Выводим на страницу разметку
 		li.innerHTML = `<span class= "text-todo"> ${item.value} </span>` +
 			'<div class="todo-buttons">' +
-				'<button class="todo-remove"></button>'+
-				'<button class="todo-complete"></button>' +
+			'<button class="todo-complete"></button>' +
 			'</div>';
-	
-		
-
-		// Локал
-	
-	
-
-
-	//Проверка куда нужно закидывать таски
+		//Проверка куда нужно закидывать таски
 		if (item.completed) {
 			todoCompleted.append(li)
-			 
 		} else {
 			todoList.append(li);
 		}
 
-		 
-		const todoComplet = li.querySelector('.todo-complete');
-		todoComplet.addEventListener('click', function () {
+		const todoCompl = li.querySelector('.todo-complete');
+		todoCompl.addEventListener('click', function () {
 			item.completed = !item.completed;
 			render();
 		})
-		
-		
-		//Удаление элемента
-		const todoRemove = li.querySelector('.todo-remove')
-		todoRemove.addEventListener('click',function(e){
-			 if(e.target.className = "todo-remove"){
-				 e.target.parentNode.parentNode.remove();
-			 }
-			
-		})
-
-		let local = JSON.parse(localStorage.getItem('taskAdd'));
-		localStorage.setItem('taskAdd',JSON.stringify(todoData));
-		console.log(local);
-	 
-
 	})
 };
 
+ 
 
 todoControl.addEventListener('submit', (e) => {
 	e.preventDefault();
+
 	//Объект который принимает значения из input
 	const newTodo = {
 		value: headerInput.value,
 		completed: false,
 
 	};
- 
-	if(newTodo.value === ''){
-		alert('Введите значение');
-	}else{
-		todoData.push(newTodo);
-		headerInput.value = "";
-	}
+	todoData.push(newTodo);
 	render();
 })
-
-
 
 render();
