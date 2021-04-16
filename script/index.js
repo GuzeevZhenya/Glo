739deb2eae16235000bd2f0606f3ff37e8c6c9d8
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-16 10:49:52 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,19 +1,32 @@
+// let result = '';
+// let items = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
+
+
+
+
+
+// 	for (let i=0;i<6;i++){
+// 		let item = items[Math.floor(Math.random() * items.length)];
+// 		result += item;
+// 	}
+
+// 	let a = "#" + result;
+// 	d
+// 	console.log(a);
+// 	result = '';
+	
+// })
+// console.log(result);
+
 let result = '';
-let items = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
 let button = document.querySelector('.btn');
 
-
-
 button.addEventListener('click', function () {
-	for (let i=0;i<6;i++){
-		let item = items[Math.floor(Math.random() * items.length)];
-		result += item;
-	}
-
-	let a = "#" + result;
-	document.body.style.background = a;
+	let red = Math.floor(Math.random(255) * 255);
+	let blue = Math.floor(Math.random(255) * 255);
+	let green = Math.floor(Math.random(255)*255);
+ 
+	let a = `rgb(${green},${red},${blue})`
 	console.log(a);
-	result = '';
-	
+	document.body.style.background = a
 })
-console.log(result);
