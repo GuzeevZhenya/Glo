fb06e75de9321930a2f4a5b9e07fbee72de873f6
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-16 13:32:34 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,32 +1,21 @@
-// let result = '';
-// let items = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
-
-
-
-
-
-// 	for (let i=0;i<6;i++){
-// 		let item = items[Math.floor(Math.random() * items.length)];
-// 		result += item;
+// let car = {
+// 	doors: 4,
+// 	turbo: false,
+// 	ride: function () {
+// 		console.log('car ride')
 // 	}
+// };
 
-// 	let a = "#" + result;
-// 	d
-// 	console.log(a);
-// 	result = '';
-	
-// })
-// console.log(result);
+// let newCar = Object.create(car);
+// newCar.model = 'mazda';
+// console.log(newCar.doors);
 
-let result = '';
-let button = document.querySelector('.btn');
+// console.log(newCar.hasOwnProperty('door'));
 
-button.addEventListener('click', function () {
-	let red = Math.floor(Math.random(255) * 255);
-	let blue = Math.floor(Math.random(255) * 255);
-	let green = Math.floor(Math.random(255)*255);
- 
-	let a = `rgb(${green},${red},${blue})`
-	console.log(a);
-	document.body.style.background = a
-})
+// console.log(car.isPrototypeOf(newCar));
+
+function Car() {
+	this.model = 'mazda';
+}
+
+let car1 = new Car();
