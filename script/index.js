1f468bc1365353ca11281f6cf856c600dccabd22
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-19 10:04:06 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,21 +1,44 @@
-// let car = {
-// 	doors: 4,
-// 	turbo: false,
-// 	ride: function () {
-// 		console.log('car ride')
-// 	}
-// };
+class DomElement {
+	constructor(selector, height, width, bg, fontSize,text) {
+		this.selector = selector;
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.text = text;
+	}
 
-// let newCar = Object.create(car);
-// newCar.model = 'mazda';
-// console.log(newCar.doors);
 
-// console.log(newCar.hasOwnProperty('door'));
 
-// console.log(car.isPrototypeOf(newCar));
+	getSelector() {
+		let body = document.querySelector('body');
+		let newElement;
+		console.log(this.selector)
+		if (this.selector.startsWith('.')) {
+			newElement = document.createElement('div');
+			newElement.classList.add(this.selector);
+			body.append(newElement);
+		}
+		else if (this.selector.startsWith('#')){
+			newElement = document.createElement('p');
+			newElement.classList.add(this.selector);
+			body.append(newElement);
+		}
+		newElement.style.cssText = `
+			height:${this.height}px;
+			width: ${this.width}px;
+      background: ${this.bg};
+      font-size: ${this.fontSize}px;
+		`
+		newElement.innerHTML = this.text
+	}
 
-function Car() {
-	this.model = 'mazda';
+ 
+
+
 }
 
-let car1 = new Car();
+let element = new DomElement('.input', 100, 100, 'red', 20,'hi world');
+element.getSelector();
+let paragrf = new DomElement('#paragraf', 40, 200, 'blue', 40,'hello');
+paragrf.getSelector();
