67eb3e1af943ef53435293dd746e34be60790719
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-19 20:19:25 +0300

    done

--- script/index.js
+++ script/index.js
@@ -5,15 +5,23 @@ let button = document.querySelector('.btn');
 
 
 button.addEventListener('click', function () {
-	for (let i=0;i<6;i++){
-		let item = items[Math.floor(Math.random() * items.length)];
-		result += item;
-	}
+	// for (let i=0;i<6;i++){
+	// 	let item = items[Math.floor(Math.random() * items.length)];
+	// 	result += item;
+	// }
 
-	let a = "#" + result;
-	document.body.style.background = a;
-	console.log(a);
-	result = '';
+	// let a = "#" + result;
+	// document.body.style.background = a;
+	// console.log(a);
+	// result = '';
+
+	let red = Math.floor(Math.random()*255).toString(16);
+	let blue = Math.floor(Math.random()*255).toString(16);
+	let green = Math.floor(Math.random()*255).toString(16);
+
+	let result = `#${red}${blue}${green}`
+	document.body.style.background = result;
+	console.log(result);
 	
 })
 console.log(result);
