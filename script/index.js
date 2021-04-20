7a3bbbf8e69ff241da8b0e42faaca852f4fda8b2
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-20 11:27:00 +0300

    done

--- script/index.js
+++ script/index.js
@@ -13,16 +13,24 @@
 // })
 // console.log(result);
 
-let result = '';
-let button = document.querySelector('.btn');
-let colorText = document.querySelector('.color-text');
-
 button.addEventListener('click', function () {
-	let red = Math.floor(Math.random(255) * 255);
-	let blue = Math.floor(Math.random(255) * 255);
-	let green = Math.floor(Math.random(255) *255);
- 
-	let a = `#${green.toString(16)}${red.toString(16)}${blue.toString(16)}`
-	colorText.textContent = a;
-	document.body.style.background = a;
+	// for (let i=0;i<6;i++){
+	// 	let item = items[Math.floor(Math.random() * items.length)];
+	// 	result += item;
+	// }
+
+	// let a = "#" + result;
+	// document.body.style.background = a;
+	// console.log(a);
+	// result = '';
+
+	let red = Math.floor(Math.random()*256).toString(16);
+	let blue = Math.floor(Math.random()*256).toString(16);
+	let green = Math.floor(Math.random()*256).toString(16);
+
+	let result = `#${red}${blue}${green}`
+	document.body.style.background = result;
+	console.log(result);
+	
 })
+console.log(result);
