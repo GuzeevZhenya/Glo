5e8a64cebb4c382bba3af78d56588ed75bcc4fe5
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-20 10:29:56 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,10 +1,6 @@
 // let result = '';
 // let items = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
 
-
-
-
-
 // 	for (let i=0;i<6;i++){
 // 		let item = items[Math.floor(Math.random() * items.length)];
 // 		result += item;
@@ -14,7 +10,6 @@
 // 	d
 // 	console.log(a);
 // 	result = '';
-	
 // })
 // console.log(result);
 
@@ -25,9 +20,9 @@ let colorText = document.querySelector('.color-text');
 button.addEventListener('click', function () {
 	let red = Math.floor(Math.random(255) * 255);
 	let blue = Math.floor(Math.random(255) * 255);
-	let green = Math.floor(Math.random(255)*255);
+	let green = Math.floor(Math.random(255) *255);
  
 	let a = `#${green.toString(16)}${red.toString(16)}${blue.toString(16)}`
 	colorText.textContent = a;
-	document.body.style.background = a
+	document.body.style.background = a;
 })
