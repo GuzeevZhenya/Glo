585fa2134040fb6b79f19a523c70b7d3f36cf349
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-21 09:16:24 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,68 +1,71 @@
-// function test(...arr) {
-// 	console.log(arr);
+ const number = [53,43,23,-234,-12,65,76,-3];
+ const names = ['Vlad','All','Victor','Kate','Alex','VicTor'];
+ const mix = ['Glo',25,true,'Acc','16',-2,null];
+ const badNum = [43,56,43,-43,23,'hi',34];
+
+//  for(let i =0;i<mix.lenght;i++){
+// 	 console.log(mix[i]);
+// }
+ 
+// mix.forEach(function (item,index,arr) {
+// 	console.log(this)
+// }, number)
+
+// for (let i = 0; i < names.length; i++){
+// 	names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
 // }
 
-// test('red', 6, 12, 'black', [], true, 9)
+// names.forEach(function (item, i, arr){
+// 	arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase();
+// })
 
+const correntName = names.map(function(item){
+	return item[0].toUpperCase() + item.slice(1).toLowerCase();
+})
 
-// const arr = ['red', 5, 12];
-// const [a, b, c] = arr;
-// console.log(a, b, c);
+console.log(correntName);
 
-// const createCar = ({ brand='bmw', model=6, color='black',colorInt}) => {
-// 	console.log(`Запущенно производство автомобиля ${brand} ${model}
-// 	Цвет кузова:${color},
-// 	цвет салона ${colorInt}
-							
-// 	`)
-// };
+let forWords = [];
+// for (let i = 0; i < mix.length; i++){
+// 	if (typeof mix[i] === 'string' && isNaN(mix[i])) {
+// 		forWords.push(mix[i]);
+// 	}
+// }
+// console.log(forWords);
 
-// createCar({
-// 	brand: 'Mazda',
-// 	model: 3,
-// 	color: 'blue',
-// 	colorInt: 'black'
-// });
+let filterWords = mix.filter((elem) => {
+	return typeof elem === 'string' && isNaN(elem);
+})
+console.log(filterWords)
 
-// const cars = ['mazda', 'bmw', 'audi'];
-// const [a1, b2, c3] = cars;
+let positiveNumbers = number.filter(item => item > 0)
+console.log(positiveNumbers);
 
-const carsModel = {
-	brand: 'Volvo',
-	models: {
-		sedan: ['s60','s90'],
-		cross: ['v60', 'v90'],
+let result = false;
+for (let i = 0; i < mix.length; i++){
+	if(typeof mix[i] === 'number'){
+		result = true;
+		break;
 	}
 }
 
-const { models: { sedan: [s1, s2], cross: [c1, c2] } } = carsModel;
-console.log(s1,s2,c1,c2)
 
-const car = 'btr';
-const cycle = 'bmx';
-const bike = 'honda';
+// let result2 = mix.some(function (item) {
+// 	return typeof item === 'number';
+// })
 
-const transport = {
-	car, cycle, bike,
-	ride:() {
-		console.log('go ride');
-	}
-}
+// let result3 = mix.every(function (item) {
+// 	return typeof item === 'number';
+// })
 
-const transport = {
-	bike: 'honda',
-	car: 'bmw',
-	cycle: 'bmx',
-}
-const newTransport = {
-	bike:'suziki',
-	quadBike:'polaris'
-}
+// console.log(result2);
 
-Object.assign(transport, newTransport);
-const currentTransport = Object.assign({}, transport);
-const currentTrans = { ...transport, ...newTransport };
+let sum2 = number.reduce((acc, item)=> {
+	return acc + item
+},0)
+ 
+console.log(sum2);
 
-const map = new Map();
-map.set('car', { brand: 'mazda', model: 3 });
-console.log(map);
+let arr = [[1, 2], [3, 4], [5, 2]];
+const res = arr.reduceRight((acc, item) => acc.concat(item));
+console.log(res);
