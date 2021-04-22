ab160befeadaa867025539c405685e68b3a4218f
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-04-22 15:43:57 +0300

    done

--- Доп задание/index.js
+++ Доп задание/index.js
@@ -1,11 +1,11 @@
- const dayTime = document.querySelector('.dayTime');
- const dayInfo = document.querySelector('.dayInfo');
- const time = document.querySelector('.time');
- const daysBeforeNewYear = document.querySelector('.daysBeforeNewYear');
- let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятницы', 'Субботы'];
- let month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const dayTime = document.querySelector('.dayTime');
+const dayInfo = document.querySelector('.dayInfo');
+const time = document.querySelector('.time');
+const daysBeforeNewYear = document.querySelector('.daysBeforeNewYear');
+let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятницы', 'Субботы'];
+let month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
- function getTimeInfo(hour) {
+function getTimeInfo(hour) {
 	if (hour > 0 && hour < 5) {
 		return 'Доброй ночи'
 	} else if (hour > 6) {
@@ -29,23 +29,34 @@ function getTimeRemaining(deadline) {
 	let dateStop = new Date(deadline).getTime();
 	let dateNow = new Date().getTime();
 	let timeRemaining = Math.floor((dateStop - dateNow) / 1000 / 60 / 60 / 24);
-
-	 
-		dayTime.textContent = `${getTimeInfo(hour)}`;
-		dayInfo.textContent = `Сегодня ${days[dayName]}`;
-		time.textContent = `Текущее время: ${hour}:${min}:${sec} ${showAmPm ? amPm : ''}`;
-		daysBeforeNewYear.textContent = `До нового года осталось ${timeRemaining} дня`;
+	return {
+		hour,min,sec,amPm,dayName,timeRemaining
+	}
 }
 
-getTimeRemaining();
-setInterval(function() { getTimeRemaining('23 jun 2021')}, 1000);
+function showInfo() {
+	let timer = getTimeRemaining('23 may 2021');
+	 
  
+	dayTime.textContent = `${getTimeInfo(timer.hour)}`;
+	dayInfo.textContent = `Сегодня ${days[timer.dayName]}`;
+	time.textContent = `Текущее время: ${timer.hour}:${timer.min}:${timer.sec} ${timer.showAmPm ? amPm : ''}`;
+	daysBeforeNewYear.textContent = `До нового года осталось ${timer.timeRemaining} дня`;
+}
 
- 
- 
+setInterval(showInfo,1000);
 
- 
+// getTimeRemaining();
+// setInterval(function () {
+// 	getTimeRemaining('23 jun 2021')
+// }, 1000);
 
 
- // setInterval(showTime('31 dec 2021'),1000);
- // setInterval(showTime('31 dec 2021'),1000);
+
+
+
+
+
+
+// setInterval(showTime('31 dec 2021'),1000);
+// setInterval(showTime('31 dec 2021'),1000);
